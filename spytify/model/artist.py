6201fd5608ef7f23daa459b068da93b2d5b5a7d1
caiopo <caiopoliveira@gmail.com>
2017-02-28 00:34:28 -0300

    Add artist.py

---
+++ spytify/model/artist.py
@@ -0,0 +1,76 @@
+from collections import namedtuple
+
+
+class CoverArt(namedtuple('CoverArt', 'height width url')):
+    pass
+
+
+class Artist:
+    def __init__(self, json, spy=None):
+        self._spy = spy
+
+        self._load_json(json)
+
+    def _load_json(self, json):
+        self._name = json['name']
+        self._id = json['id']
+        self._uri = json['uri']
+        self._url = json['external_urls']['spotify']
+
+        self._genres = json.get('genres')
+        self._popularity = json.get('popularity')
+        self._followers = json.get('followers').get('total')
+
+        if 'images' in json:
+            self._images = [CoverArt(**image) for image in json['images']]
+        else:
+            self._images = None
+
+    def _load(self):
+        json = self._spy._get('artists', self.id)
+
+        self._load_json(json)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def uri(self):
+        return self._uri
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def genres(self):
+        if self._genres is None:
+            self._load()
+
+        return self._genres
+
+    @property
+    def popularity(self):
+        if self._popularity is None:
+            self._load()
+
+        return self._popularity
+
+    @property
+    def followers(self):
+        if self._followers is None:
+            self._load()
+
+        return self._followers
+
+    @property
+    def images(self):
+        if self._images is None:
+            self._load()
+
+        return self._images
--- spytify/spotify.py
+++ spytify/spotify.py
@@ -1,11 +1,49 @@
 from requests_oauthlib import OAuth2Session
+from .model import Artist
+
+import re
+
+URI_REGEX = re.compile(r'spotify:(.+):(.+)')
+
+
+def clean_uri(uri):
+    match = URI_REGEX.match(uri)
+
+    try:
+        return match.group(2)
+    except AttributeError:
+        return uri
 
 
 class Spotify:
 
-    def __init__(self, session=None):
-        self._session = session or OAuth2Session()
+    API_URL = 'https://api.spotify.com/v1/'
+
+    def __init__(self, authorization_code=None,
+                 client_credentials=None, session=None):
+        if authorization_code:
+            pass
+        elif client_credentials:
+            client_credentials.fetch_token()
+            self._session = client_credentials.session
+        else:
+            self._session = session or OAuth2Session()
+
+    def _get(self, *endpoint, params=None, **kwargs):
+        kwargs.update(params or {})
+
+        if isinstance(endpoint, tuple):
+            endpoint = '/'.join(endpoint)
+
+        return self._session.get(self.API_URL + endpoint).json()
 
     def test(self):
         return self._session.get(
             'https://api.spotify.com/v1/audio-features/06AKEBrKUckW0KREUWRnvT')
+
+    def track(self, track_id):
+        return self._get('tracks', clean_uri(track_id))
+
+    def artist(self, artist_id):
+        json = self._get('artists', clean_uri(artist_id))
+        return Artist(json, spy=self)
